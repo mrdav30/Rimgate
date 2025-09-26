a7fddd5fa45d9bf194e8979497e8eba11c0600ef
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-09-26 00:27:03 -0400

    fix: meatlab graphic changing if plant is not meatplant

--- Source/Rimgate/Things/Building_MeatLab.cs
+++ Source/Rimgate/Things/Building_MeatLab.cs
@@ -10,15 +10,24 @@ namespace Rimgate;
 
 public class Building_MeatLab : Building_PlantGrower
 {
-    private static Dictionary<string, Graphic> growGraphics = new();
+    private static Dictionary<string, Graphic> _growGraphics = new();
 
-    private CompPowerTrader _power;
+    public CompPowerTrader PowerTrader
+    {
+        get
+        {
+            _cachedPowerTrader ??= GetComp<CompPowerTrader>();
+            return _cachedPowerTrader;
+        }
+    }
 
-    private Plant _plant;
+    private CompPowerTrader _cachedPowerTrader;
+
+    private Plant_MeatPlant _plant;
 
     static Building_MeatLab()
     {
-        if (Building_MeatLab.growGraphics.Any())
+        if (Building_MeatLab._growGraphics.Any())
             return;
 
         string[] growStates = { "empty", "25%", "50%", "75%", "100%" };
@@ -39,26 +48,23 @@ public class Building_MeatLab : Building_PlantGrower
                 null);
 
             graphic.Init(request);
-            growGraphics.Add(growState, graphic);
+            _growGraphics.Add(growState, graphic);
         }
-    }
-
-    public override void SpawnSetup(Map map, bool respawningAfterLoad)
-    {
-        base.SpawnSetup(map, respawningAfterLoad);
-
-        _power = base.GetComp<CompPowerTrader>();
     }
 
     public override void TickRare()
     {
-        if (_power == null 
-            || _power.PowerNet == null
-            || !PlantsOnMe.Any()) return;
-
-        _plant = PlantsOnMe.First() ?? null;
-
         base.TickRare();
+
+        if(_plant != null) return;
+        foreach(Plant plant in PlantsOnMe)
+        {
+            if(plant is Plant_MeatPlant pmp)
+            {
+                _plant = pmp;
+                break;
+            }         
+        }
     }
 
     public override Graphic Graphic
@@ -67,19 +73,19 @@ public class Building_MeatLab : Building_PlantGrower
         {
             // For when it's minified or in a trade ship.
             if (_plant == null)
-                return Building_MeatLab.growGraphics["empty"];
+                return Building_MeatLab._growGraphics["empty"];
 
             var growthPercent = Mathf.FloorToInt((_plant.Growth + 0.0001f) * 100f);
             if (growthPercent <= 10)
-                return Building_MeatLab.growGraphics["empty"];
+                return Building_MeatLab._growGraphics["empty"];
             else if (growthPercent <= 25)
-                return Building_MeatLab.growGraphics["25%"];
+                return Building_MeatLab._growGraphics["25%"];
             else if (growthPercent <= 50)
-                return Building_MeatLab.growGraphics["50%"];
+                return Building_MeatLab._growGraphics["50%"];
             else if (growthPercent <= 75)
-                return Building_MeatLab.growGraphics["75%"];
+                return Building_MeatLab._growGraphics["75%"];
             else
-                return Building_MeatLab.growGraphics["100%"];
+                return Building_MeatLab._growGraphics["100%"];
         }
     }
 
@@ -87,7 +93,7 @@ public class Building_MeatLab : Building_PlantGrower
     {
         string text = base.GetInspectString();
 
-        if (base.Spawned && _plant != null)
+        if (Spawned && _plant != null)
         {
             if (!text.NullOrEmpty())
                 text += "\n";
