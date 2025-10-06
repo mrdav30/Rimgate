bf36705642f2b406aed79daa4ebc66a3873c8ae8
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-10-06 14:31:11 -0400

    fix: wraith buckler shield bubble tick

---
+++ Source/Rimgate/Things/Apparel_WraithForceBuckler.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using VEF.Apparels;
+using Verse;
+
+namespace Rimgate;
+
+public class Apparel_WraithForceBuckler : Apparel_Shield
+{
+    protected override void Tick()
+    {
+        if (Wearer == null || !Wearer.Spawned || Wearer.Destroyed)
+            return;
+
+        base.Tick();
+    }
+}
