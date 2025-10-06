9f66a550123aed686dfaee3f5223985816d3d202
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-10-06 14:58:44 -0400

    update: added unrecruitable hediff

---
+++ Source/Rimgate/Hediffs/Comps/HediffComp_Unrecruitable.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimgate;
+
+/// <summary>
+/// While present, this pawn cannot be recruited.
+/// </summary>
+public class HediffComp_Unrecruitable : HediffComp
+{
+    public override void CompPostPostAdd(DamageInfo? dinfo)
+    {
+        base.CompPostPostAdd(dinfo);
+        Enforce();
+    }
+
+    public override void CompPostTick(ref float severityAdjustment)
+    {
+        if (parent.pawn.IsHashIntervalTick(500)) // ~8.2s
+            Enforce();
+    }
+
+    private void Enforce()
+    {
+        var pawn = parent.pawn;
+        var guest = pawn?.guest;
+        if (guest == null) return;
+        guest.Recruitable = false;
+    }
+}
