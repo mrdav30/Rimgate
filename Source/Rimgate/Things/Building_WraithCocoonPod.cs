204a6d9af3595e33c3aa3d5e1194da239994ae60
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-10-15 23:53:35 -0400

    update: wraith cocoon pod - reorganized

---
+++ Source/Rimgate/Things/Building_WraithCocoonPod.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace RimWorld;
+
+public class Building_WraithCocoonPod : Building_CryptosleepCasket
+{
+    public override IEnumerable<FloatMenuOption> GetFloatMenuOptions(Pawn myPawn) => Enumerable.Empty<FloatMenuOption>();
+
+    public override IEnumerable<Gizmo> GetGizmos()
+    {
+        yield break;
+    }
+}
