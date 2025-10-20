bf1f379482b92c1ef26586597ddd75ccc4adfcaa
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-10-20 11:14:22 -0400

    update: use harmony patch for wraith psycasts - kull unrecruitable

---
+++ Source/Rimgate/HarmonyPatches/Harmony_Psycast.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Rimgate.HarmonyPatches;
+
+// allow wraith to use all psycast abilities
+[HarmonyPatch(typeof(Ability), nameof(Ability.AICanTargetNow))]
+public static class Harmony_Psycast
+{
+    public static bool Prefix(
+        Ability __instance,
+        LocalTargetInfo target,
+        ref bool __result)
+    {
+        if (__instance is not Psycast || !__instance.pawn.IsXenoTypeOf(RimgateDefOf.Rimgate_Wraith)) return true;
+
+        __result = false;
+        if (!__instance.CanCast) return false;
+
+        if (!__instance.CanApplyOn(target)) return false;
+
+        if (__instance.EffectComps != null)
+        {
+            foreach (CompAbilityEffect effectComp in __instance.EffectComps)
+            {
+                if (!effectComp.AICanTargetNow(target))
+                    return false;
+            }
+        }
+
+        __result = true;
+        return true;
+    }
+}
