33d51fed06c4f0f4b54e80ad349da4518620f854
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2025-10-28 13:54:51 -0400

    update: make replacement dhd require analysis

---
+++ Source/Rimgate/Comps/Properties/CompProperties_AnalyzableResearchWhen.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rimgate;
+
+public class CompProperties_AnalyzableResearchWhen : CompProperties_CompAnalyzableUnlockResearch
+{
+    public ResearchProjectDef requiresResearchDef;
+
+    public bool hideWhenDone = true;
+
+    public CompProperties_AnalyzableResearchWhen() => compClass = typeof(CompAnalyzableResearchWhen);
+}
