e04068ace7f57b54f0981a57e52c7a81f1297497
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2026-01-15 18:17:12 -0500

    update: block messages that originate from gate sites

---
+++ Source/Rimgate/HarmonyPatches/Harmony_Messages_Message.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Runtime.CompilerServices;
+using Verse;
+
+namespace Rimgate.HarmonyPatches;
+
+// <summary>
+// Suppresses messages that would target GateQuestSite world objects.
+// </summary>
+[HarmonyPatch(typeof(Messages))]
+public static class Harmony_Messages_Message
+{
+    [HarmonyPatch(typeof(Messages), "Message", new Type[] { typeof(string), typeof(LookTargets), typeof(MessageTypeDef), typeof(Quest), typeof(bool) })]
+    [HarmonyPrefix]
+    public static bool Prefix(string text, LookTargets lookTargets, MessageTypeDef def, Quest quest, bool historical)
+    {
+        GlobalTargetInfo target = lookTargets?.TryGetPrimaryTarget() ?? GlobalTargetInfo.Invalid;
+        if (!ShouldSkip(target))
+            return true;
+
+        return false;
+    }
+
+    [HarmonyPatch(typeof(Messages), "Message", new Type[] { typeof(string), typeof(LookTargets), typeof(MessageTypeDef), typeof(bool) })]
+    [HarmonyPrefix]
+    public static bool Prefix(string text, LookTargets lookTargets, MessageTypeDef def, bool historical)
+    {
+        GlobalTargetInfo target = lookTargets?.TryGetPrimaryTarget() ?? GlobalTargetInfo.Invalid;
+        if (!ShouldSkip(target))
+            return true;
+
+        return false;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool ShouldSkip(GlobalTargetInfo info)
+    {
+        if (!info.IsValid)
+            return false;
+        MapParent mapParent = info.Map?.Parent ?? null;
+        if (mapParent == null || mapParent is not WorldObject_GateQuestSite)
+            return false;
+
+        if (RimgateMod.Debug)
+            Log.Message($"[Rimgate] Suppressed message for GateQuestSite");
+
+        return true;
+    }
+}
