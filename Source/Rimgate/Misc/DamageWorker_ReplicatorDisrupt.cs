74589a1fcb76b32ce91c775c82d5e1112096bed0
DESKTOP-5KM6RA3\david <david.oravsky@gmail.com>
2026-02-19 13:28:34 -0500

    update: refactored replicator disrupter - balanced biomass costs

---
+++ Source/Rimgate/Misc/DamageWorker_ReplicatorDisrupt.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace Rimgate;
+
+public class DamageWorker_ReplicatorDisrupt : DamageWorker_AddInjury
+{
+    public override DamageResult Apply(DamageInfo dinfo, Thing victim)
+    {
+        // Only stuns and damages mechanoids
+        if (victim == null || victim is not Pawn p || !p.RaceProps.IsMechanoid) return new DamageResult();
+
+
+        DamageResult damageResult = base.Apply(dinfo, victim);
+        damageResult.stunned = true;
+        return damageResult;
+    }
+}
